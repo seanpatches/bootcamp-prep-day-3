de5c9856c34dd3f347ed2aa66114978fb1715a71
Sean Nelson <smnelsonn@gmail.com>
2019-01-16 20:42:04 -0800

    finished day 3, JS and HTML interaction

--- main.js
+++ main.js
@@ -1,2 +1,57 @@
-var sum = (3+4)
-console.log('Hello Console!', sum);
+var x = 15
+
+if(x>12){
+    console.log(x);
+    console.log('grooving with if!');
+}
+
+if(x=15){
+    console.log('you did it!');
+}
+
+else{
+    console.log('too small!');
+}
+
+//1. Initialize
+//2. Condition
+//3. Post Loop Action
+for(var i=0; i<x; i++){
+
+/// Prints all numbers that are EVEN
+    if( i%2 === 0){
+        console.log(i);
+    }
+}
+
+
+///CREATES FUNCTION
+function sayHello(){
+    console.log('hi');
+}
+
+if(x % 2 === 0){
+    sayHello()
+}
+
+///eslint-disable-next line
+
+///Function to obtain number from input and print even or odd
+function evenOrOdd(){
+    var input = document.getElementById('number');
+    console.log(input.value);
+
+    var y = input.value;
+
+    if(y % 2 === 0){
+        console.log('even')
+}
+
+    else {
+        console.log('odd');
+    }
+}
+
+//// NAME OF FUNCTION +paranthesis RUNS THE FUNCTION
+sayHello();
+
